aa34663e7a8265cde47af7ebcb0753fa121318b4
Keith A. Lewis <kal@kalx.net>
2021-09-15 02:47:39 -0400

    initial commit

--- xll_template.cpp
+++ xll_template.cpp
@@ -1,65 +1,75 @@
 ﻿// xll_template.cpp - Sample xll project.
-#include <cmath> // for double tgamma(double)
 #include "xll_template.h"
 
 using namespace xll;
-
-AddIn xai_tgamma(
-	// Return double, C++ name of function, Excel name.
-	Function(XLL_DOUBLE, "xll_tgamma", "TGAMMA")
-	// Array of function arguments.
-	.Arguments({
-		Arg(XLL_DOUBLE, "x", "is the value for which you want to calculate Gamma.")
-	})
-	// Function Wizard help.
-	.FunctionHelp("Return the Gamma function value.")
-	// Function Wizard category.
-	.Category("MATH")
-	// URL linked to `Help on this function`.
-	.HelpTopic("https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/tgamma-tgammaf-tgammal")
-	.Documentation(R"xyzyx(
-The <i>Gamma</i> function is \(\Gamma(x) = \int_0^\infty t^{x - 1} e^{-t}\,dt\), \(x \ge 0\).
-If \(n\) is a natural number then \(\Gamma(n + 1) = n! = n(n - 1)\cdots 1\).
-<p>
-Any valid HTML using <a href="https://katex.org/" target="_blank">KaTeX</a> can 
-be used for documentation.
-)xyzyx")
-);
-// WINAPI calling convention must be specified
-double WINAPI xll_tgamma(double x)
-{
-#pragma XLLEXPORT // must be specified to export function
-
-	return tgamma(x);
-}
-
-// Press Alt-F8 then type 'XLL.MACRO' to call 'xll_macro'
-// See https://xlladdins.github.io/Excel4Macros/
-AddIn xai_macro(
-	// C++ function, Excel name of macro
-	Macro("xll_macro", "XLL.MACRO")
-);
-// Macros must have `int WINAPI (*)(void)` signature.
-int WINAPI xll_macro(void)
-{
-#pragma XLLEXPORT
-	// https://xlladdins.github.io/Excel4Macros/reftext.html
-	// A1 style instead of default R1C1.
-	OPER reftext = Excel(xlfReftext, Excel(xlfActiveCell), OPER(true));
-	// UTF-8 strings can be used.
-	Excel(xlcAlert, OPER("XLL.MACRO called with активный 细胞: ") & reftext);
-
-	return TRUE;
-}
-
-#ifdef _DEBUG
+using namespace FRE6233;
 
 // Create XML documentation and index.html in `$(TargetPath)` folder.
 // Use `xsltproc file.xml -o file.html` to create HTML documentation.
+#ifdef _DEBUG
 Auto<Open> xao_template_docs([]() {
 
-	return Documentation("MATH", "Documentation for the xll_template add-in.");
+	return Documentation(CATEGORY, "Documentation for " CATEGORY ".");
 
 });
-
 #endif // _DEBUG
+
+AddIn xai_option_moneyness(
+	Function(XLL_DOUBLE, "xll_option_moneyness", "OPTION.MONEYNESS")
+	.Arguments({
+		Arg(XLL_DOUBLE, "f", "is the forward."),
+		Arg(XLL_DOUBLE, "s", "is the vol."),
+		Arg(XLL_DOUBLE, "k", "is the strike."),
+		})
+	.FunctionHelp("Return the option moneyness.")
+	.Category(CATEGORY)
+	.Documentation(R"(
+Moneyness is \((\log(k/f) + s^2/2)/s\).
+)")
+);
+double WINAPI xll_option_moneyness(double f, double s, double k)
+{
+#pragma XLLEXPORT
+	return option::moneyness(f, s, fabs(k));
+}
+
+AddIn xai_option_value(
+	Function(XLL_DOUBLE, "xll_option_value", "OPTION.VALUE")
+	.Arguments({
+		Arg(XLL_DOUBLE, "f", "is the forward."),
+		Arg(XLL_DOUBLE, "s", "is the vol."),
+		Arg(XLL_DOUBLE, "k", "is the strike."),
+		})
+	.FunctionHelp("Return the option call (k > 0) or put (k < 0) value.")
+	.Category(CATEGORY)
+	.Documentation(R"(
+Option value is \(E[\max\{F - k, 0\}]\) for a call
+and \(E[\max\{k - F, 0\}]\) for a put.
+)")
+);
+double WINAPI xll_option_value(double f, double s, double k)
+{
+#pragma XLLEXPORT
+	// c - p = f - k
+	return k < 0 ? option::value(f, s, -k) : option::value(f, s, k) + f - k;
+}
+
+AddIn xai_option_delta(
+	Function(XLL_DOUBLE, "xll_option_delta", "OPTION.DELTA")
+	.Arguments({
+		Arg(XLL_DOUBLE, "f", "is the forward."),
+		Arg(XLL_DOUBLE, "s", "is the vol."),
+		Arg(XLL_DOUBLE, "k", "is the strike."),
+		})
+	.FunctionHelp("Return the option call (k > 0) or put (k < 0) delta.")
+	.Category(CATEGORY)
+	.Documentation(R"(
+Option delta is the derivative of option value with respect to forward.
+)")
+);
+double WINAPI xll_option_delta(double f, double s, double k)
+{
+#pragma XLLEXPORT
+	// dc - dp = 1
+	return k < 0 ? option::delta(f, s, -k) : 1 + option::delta(f, s, k);
+}
--- xll_template.h
+++ xll_template.h
@@ -1,5 +1,89 @@
 // xll_template.h - common includes
 #pragma once
-// Uncomment to build for pre 2007 Excel
-//#define XLOPERX XLOPER
+#include <cmath> // erf
+#include <limits>
 #include "xll/xll/xll.h"
+
+#ifndef CATEGORY
+#define CATEGORY "FRE6233"
+#endif
+
+namespace FRE6233 {
+
+	// Return NaN to indicate error.
+	constexpr double NaN = std::numeric_limits<double>::quiet_NaN();
+	// sqrt(2 pi)
+	constexpr double M_SQRT2PI = 2.50662827463100050240;
+	//#ifndef M_SQRT2
+	// sqrt(2)
+	constexpr double M_SQRT2 = 1.41421356237309504880;
+	//#endif
+
+	namespace normal {
+
+		// P(X <= x) and derivatives
+		inline double cdf(double x, int n = 0) 
+		{
+			if (n == 0) {
+				return  (1 + ::erf(x / M_SQRT2)) / 2;
+			}
+			double phi = ::exp(-x * x / 2) / M_SQRT2PI;
+			if (n == 1) {
+				return phi;
+			}
+			//!!! Hermite polynomials
+
+			return NaN;
+		}
+		// P_s(X <= x) = P(X <= x - s)
+		inline double cdf(double x, double s, int nx = 0, int ns = 0)
+		{
+			return cdf(x - s, nx + ns) * (ns % 2 ? -1 : 1);
+		}
+
+		// kappa(s) = log E[e^{sX}] = s^2/2 and derivativs
+		inline double cumulant(double s, int n = 0)
+		{
+			if (n == 0) {
+				return s * s / 2;
+			}
+			if (n == 1) {
+				return s;
+			}
+			if (n == 2) {
+				return 1;
+			}
+
+			return 0;
+		}
+	}
+
+	namespace option {
+		inline double moneyness(double f, double s, double k)
+		{
+			if (f <= 0 || s <= 0 || k <= 0) {
+				return NaN;
+			}
+
+			return (log(k / f) + normal::cumulant(s)) / s;
+		}
+
+		// put option value
+		inline double value(double f, double s, double k)
+		{
+			double m = moneyness(f, s, k);
+
+			return k * normal::cdf(m) - f * normal::cdf(m, s);
+		}
+
+		// put option delta
+		inline double delta(double f, double s, double k)
+		{
+			double m = moneyness(f, s, k);
+
+			return -normal::cdf(m, s);
+		}
+
+	}
+
+}
