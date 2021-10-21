559cd40214d0ef8fd34a9bcc003bf5b60c8a79bd
keithalewis <kal@kalx.net>
2021-10-21 12:34:07 -0400

    docs

--- xll_option.cpp
+++ xll_option.cpp
@@ -12,6 +12,7 @@ using namespace fms;
 // Create XML documentation and index.html in `$(TargetPath)` folder.
 // Use `xsltproc file.xml -o file.html` to create HTML documentation.
 #ifdef _DEBUG
+xll_url_set FRE6233("https://keithalewis.github.io/FRE6233/");
 Auto<Open> xao_template_docs([]() {
 
 	return Documentation(CATEGORY, "Documentation for " CATEGORY ".");
