26a0b5986f1b190fbaef9bf5246ae207a97c189d
Keith A. Lewis <kal@kalx.net>
2021-11-04 11:34:12 -0400

    sync

--- fms_binomial.h
+++ fms_binomial.h
@@ -8,19 +8,19 @@
 
 namespace fms::binomial {
 
-	// e^{-rt(n - j)/n}E[nu(S_n) | W_j = i], V_j = j - 2 W_j
-	// S_n = S e^{rt + sigma sqrt(t)/sqrt(n) V_n}/cosh(sigma sqrt(t)/sqrt(n))^n
-	inline double value(int i, int j, int n, double r, double S, double sigma, double t, const std::function<double(double)>& nu, bool american = false)
+	// F_j = F e^{s V_j/sqrt(n)}/cosh(s/sqrt(n))^n, t_j = tj/n
+	// v_j(i) = E_j[nu(F_n) | F_j(i) = S, tau >= t_j]
+	inline double value(int i, int j, int n, double f, double s, 
+		const std::function<double(double)>& nu, bool american = false)
 	{
-		double s = sigma * sqrt(t) / sqrt(n);
-		double S_j = S * exp(r * t * j / n + s * (j - 2. * i)) / pow(cosh(s), j);
+		double F_j = f * exp(s * (j - 2. * i)) / pow(cosh(s), j);
 
 		if (j == n) {
-			return nu(S_j);
+			return nu(F_j);
 		}
 
-		double v = value(i, j + 1, n, r, S * exp(r * t / n), sigma, t - 1./n, nu, american) / 2
-			+ value(i + 1, j + 1, n, r, S * exp(r * t / n), sigma, t - 1./n, nu, american) / 2;
+		double v = (value(i, j + 1, n, f, s, nu, american)
+			      + value(i + 1, j + 1, n, f, s, nu, american)) / 2;
 
 		if (american) {
 			//!!! optimal exercise code goes here !!!
@@ -30,38 +30,21 @@ namespace fms::binomial {
 	}
 
 	// American put (k < 0) or call (p > 0) value at time j given W_j = i
-	inline double value(int i, int j, int n, double r, double S, double sigma, double k, double t, bool american = false)
+	inline double value(int i, int j, int n, double r, double S, double sigma, int pc, double k, double t, bool american = false)
 	{
 		std::function<double(double)> nu;
 
-		if (k < 0) { // put
-			nu = [k](double F) { return std::max(-k - F, 0.); };
+		if (pc == 'P') { // put
+			nu = [k](double F) { return std::max(k - F, 0.); };
 		}
-		else if (k > 0) { // call
+		else if (pc == 'C') { // call
 			nu = [k](double F) { return std::max(F - k, 0.); };
 		}
 		else {
-			return signbit(k) ? 0 : f;
+			return std::numeric_limits<double>::quiet_NaN();
 		}
 
-		return value(i, j, n, f, s, nu, american);
-	}
-
-	// American B-S/M put or call value at time j given W_j = i, where S_j = e^{-rtj/n} F_j
-	inline double value(int i, int j, int n, double r, double S, double sigma, int c, double k, double t, bool american = false)
-	{
-		double Dn = exp(-r * t * (n - j) / n);
-		double Dj = exp(-r * t * j / n);
-		double s = sigma * sqrt(t);
-
-		if (c == 'P') {
-			return Dn * value(i, j, n, S / Dj, s, -k, american);
-		}
-		else if (c == 'C') {
-			return Dn * value(i, j, n, S / Dj, s, k, american);
-		}
-
-		return std::numeric_limits<double>::quiet_NaN();
+		return value(i, j, n, r, S, sigma, t, nu, american);
 	}
 
 } // namespace fms
