3bf675e4fce2e0c62d06e65120f4ecfc933fbff0
luvudidadu <52876612+luvudidadu@users.noreply.github.com>
2021-12-01 23:25:34 +0800

    Update fms_option.t.cpp

--- fms_option.t.cpp
+++ fms_option.t.cpp
@@ -33,6 +33,27 @@ double monte_carlo_option_value(double f, double s, double k, size_t n = 10000)
 	return monte_carlo::average(n, p);
 }
 
+double monte_carlo_option_variance(double f, double s, double k, size_t n = 10000)
+{
+	std::function<double(double)> payoff;
+	if (k < 0) {
+		payoff = [k](double x) { return std::pow(std::max(-k - x, 0.),2); };
+	}
+	else {
+		payoff = [k](double x) { return std::pow(std::max(x - k, 0.),2); };
+	}
+
+	std::default_random_engine dre;
+	std::normal_distribution<double> X;
+	auto p = [f, s, &payoff, &X, &dre]() {
+		double F = f * exp(s * X(dre) - s * s / 2);
+
+		return payoff(F);
+	};
+
+	return monte_carlo::average(n, p) - std::pow(monte_carlo_option_value(f, s, k, n), 2);
+}
+
 // common to all tests
 variate::normal N;
 double fs[] = { 80, 90, 100, 110, 120 };
@@ -59,12 +80,38 @@ int option_value_test()
 
 	return 0;
 }
+
+int option_variance_test()
+{
+	double sd = 2; // two standard deviations
+	for (int f_num = 0; f_num < 5; f_num++) {
+		double f = fs[f_num];
+		for (int k_num = 0; k_num < 10; k_num++) {
+			double k;
+			if (k_num < 5) k = ks[k_num];
+			else k = -ks[k_num - 5];
+			for (int s_num = 0; s_num < 4; s_num++) {
+				double s = ss[s_num];
+				double stdev = sqrt(option::black::variance(N, f, s, 0));
+				int n = is[0];
+				double v = option::black::variance(N, f, s, k);
+				double vn = monte_carlo_option_variance(f, s, k, n);
+				double sd = 2;
+				assert(fabs(v - vn) <= stdev * sd / sqrt(n));
+
+			}
+		}
+	}
+
+	return 0;
+}
+
 int option_value_test_ = option_value_test();
 
 int option_delta_test_ = 0;
 int option_gamma_test_ = 0;
 int option_vega_test_ = 0;
 int option_implied_test_ = 0;
-int option_variance_test_ = 0;
+int option_variance_test_ = option_variance_test();
 
 #endif // _DEBUG
